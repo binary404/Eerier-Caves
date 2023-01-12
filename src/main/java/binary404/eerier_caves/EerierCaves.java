fa1bc0498b4cc84658dee79393b0736d4d1c1713
binary404 <35374899+binary404@users.noreply.github.com>
2023-01-11 19:49:35 -0600

    Gradle setup

--- src/main/java/binary404/eerier_caves/EerierCaves.java
+++ src/main/java/binary404/eerier_caves/EerierCaves.java
@@ -1,43 +1,26 @@
 package binary404.eerier_caves;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(EerierCaves.MODID)
 public class EerierCaves {
 
     // Define mod id in a common place for everything to reference
     public static final String MODID = "eerier_caves";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger(MODID);
     public EerierCaves() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
     }
 
-    @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
+
     }
 }
